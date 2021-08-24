0d523be9bea8bc2a5c71826027adb06b6713460f
Usman Mustapha <81072607+musty125@users.noreply.github.com>
2021-08-24 11:43:50 +0100

    Create tomi.js

---
+++ tomi.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+   document.getElementById('simmy').style.display='none';
+}
+$(document).ready(function(){
+   $(".fine").click(function(){
+      $("p").html("Now You Know! This is Tomilayo Gbadamosi and she is the most beautiful girl in the whole world");
+   });
+})
+$(document).ready(function(){
+   $(".fine").click(function(){
+      $("button").hide();
+   });
+})
+$(document).ready(function(){
+   $(".fine").click(function(){
+      $("#simmy").show();
+   });
+})
